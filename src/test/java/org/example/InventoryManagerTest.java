243c2636351897a58253617ca9baf248c3996bf6
Ivan Patrick Frondozo <ivan-p.frondozo@GXCHLAP00704.local>
2025-07-09 12:42:08 +0800

    Added InventoryManagerTest.java with empty methods for unit testing initializing

---
+++ src/test/java/org/example/InventoryManagerTest.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryManagerTest {
+
+    InventoryManager inventory;
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test //addProduct method
+    public void shouldAddItemWithCorrectQuantity() {
+
+    }
+
+    @Test //addProduct method
+    public void shouldNotAddItemWithQuantityZero() {
+
+    }
+
+    @Test //checkProduct method
+    public void shouldReturnExistingProduct() {
+
+    }
+
+    @Test //checkProduct method
+    public void shouldReturnProductNotFound() {
+
+    }
+
+    @Test //updateProduct method
+    public void shouldUpdateExistingProductWithValidQuantity() {
+
+    }
+
+    @Test //updateProduct method
+    public void shouldNotUpdateNonexistingProduct() {
+
+    }
+
+    @Test //removeProduct method
+    public void shouldRemoveExistingProduct() {
+
+    }
+
+    @Test //removeProduct method
+    public void shouldNotRemoveNonexistingProduct() {
+
+    }
+}
